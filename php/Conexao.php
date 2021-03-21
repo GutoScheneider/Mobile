0005842030c128d4aaa28d51cc877a901ee4e189
João Augusto Scheneider <Jascheneider@gmail.com>
2021-03-21 14:44:01 -0300

    media query

---
+++ php/Conexao.php
@@ -0,0 +1,9 @@
+<?php
+session_status();
+$conexao = null;
+try{
+    $conexao = new PDO("mysql:dbname=afc;host=localhost","root","");
+
+}catch (PDOException $erro){
+    echo "ERRO DE CONEXÃO".$erro->getMessage();
+}
